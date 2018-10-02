5bd9527212c06f4c57e352379cc088e179a1ed69
Hulst,Joep J.G.M. van <joep.vanhulst@student.fontys.nl>
2018-10-02 15:56:09 +0200

    quizRetrieval working it returns a quiz object

--- src/api/infoland/infolandApi.ts
+++ src/api/infoland/infolandApi.ts
@@ -1,7 +1,45 @@
+import { callbackify } from "util";
+
 let axios = require('axios');
+class answer
+{
+    id: string;
+    text: string;
+    constructor(id:string,text:string)
+    {
+        this.id = id;
+        this.text = text;
+    }
+}
 class question
 {
-
+    id:string;
+    media: string;
+    answers: Array<answer> = [];
+    text: string;
+    constructor(id: string,text:string)
+    {
+        this.id = id;
+    }
+    public add(ans: answer)
+    {
+        this.answers.push(ans);
+    }
+}
+class quizObject
+{
+    id: string;
+    questions: Array<question> = [];
+    title: string;
+    constructor(id: string,title:string)
+    {
+        this.id = id;
+        this.title = title;
+    }
+    public add(quest:question)
+    {
+        this.questions.push(quest);
+    }
 }
 export class InfolandAPI
 {
@@ -18,7 +56,7 @@ export class InfolandAPI
             "username": username,
             "password": password
         })
-        .then((response: string)=>
+        .then((response: any)=>
         {
             this.token = response.data;
             cb(false,this.token);
@@ -36,16 +74,54 @@ export class InfolandAPI
                 Authorization: 'Bearer '+this.token, 
             }
         })
-        .then((response: string)=>
+        .then((response: any)=>
         {
-            console.log(response.data.pages);
+            let questiondata = response.data.pages
+            let quiz = new quizObject(response.data.id,response.data.title);
+            var i:number;
+            var j:number;
+            for(i = 0; i<Object.keys(questiondata).length;i++){
+                var answerdata = questiondata[i].answers;
+                var quest = new question(questiondata[i].id,questiondata[i].questionBase);
+                quest.media = questiondata[i].media.id;
+                //console.log(answerdata);
+                for(j = 0; j<Object.keys(answerdata).length;j++)
+                {
+                    //console.log(i+" , "+ j);
+                    var ans = new answer(answerdata[j].id,answerdata[j].text)
+                    quest.add(ans);
+                }
+                quiz.add(quest);
+            }
+            //console.log(quiz.questions);
+
         })
         .catch((error:string)=>
         {
             console.log(error);
         });
     }
+    public checkanswer(quizID : string,questionID:string,answerID: string)
+    {
+        console.log(this.url+'/api/learnmaterial/'+quizID+'/update/'+questionID);
+        axios.post(this.url+'/api/learnmaterial/'+quizID+'/update/'+questionID,{
+            headers:{
+                Authorization: 'Bearer '+this.token,
+            },
+            confirmed: true,
+            answer:{answerID},
+            time:0,
+        })
+        .then((response: any)=>{
+            //console.log(response);
+        })
+        .catch((error:string)=>{
+            console.log(error);
+        })
+        
+    }
 }
 let test = new InfolandAPI('https://pubquiz.iqualify.nl');
 test.tokenRetrieval('heer','test',(error,token)=>{test.quizRetrieval('e5e7e82f-44e3-4e6d-86cf-b8fd2f9d07f2')});
+//setTimeout(function(){test.checkanswer("e5e7e82f-44e3-4e6d-86cf-b8fd2f9d07f2","68296ce1-6f88-435e-9c03-d1e247d6d4c2","341b191d-39c9-4fd4-b54b-a10944ab880c")},1000);
 
