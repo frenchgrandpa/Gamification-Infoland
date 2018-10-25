63bb943024cc807bdda031b4ac1acb004dab9d49
Frenken,Martin M.T <martin.frenken@student.fontys.nl>
2018-10-25 14:43:24 +0200

    AnswerButton testje

---
+++ src/front/hot-potato/AnswerButton.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="button">
+  <button>knoppie</button>
+    <p>hoi</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnswerButton",
+  data() {
+    return {
+     
+    };
+  }
+};
+</script>
+
+<style>
+
+
+#button {
+  height: 100%;
+  width: 110%;
+}
+</style>
