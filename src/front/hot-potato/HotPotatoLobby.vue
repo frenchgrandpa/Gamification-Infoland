7a3ad14544b05e0b073ee4430a5f37b070861d68
Dekker,Lars L <lars.dekker@student.fontys.nl>
2018-11-01 13:23:58 +0100

    Add TODO's

--- src/front/hot-potato/HotPotatoLobby.vue
+++ src/front/hot-potato/HotPotatoLobby.vue
@@ -2,14 +2,30 @@
     <div class="container">
         <h1>{{msg}}</h1>
         <p>{{currentRoute}}</p>
-        <div v-if="lobbys.length">
-            <div v-for="lobby in lobbys">
-                <v-btn v-on:click="greet">{{lobby.name}} --- Online: {{lobby.currentplayers}}</v-btn>
-            </div>
-        </div>
-        <div v-else>
-            <p>No available lobbys</p>
-        </div>
+        <v-form v-model="form_info">
+            <v-text-field
+            v-model="name"
+            :counter="10"
+            label="Name"
+            required
+            ></v-text-field>
+            <v-select
+            v-model="selectedLobby"
+            :items="lobbys"
+            :rules="[v => !!v || 'Item is required']"
+            item-value="text"
+            label="Lobby"
+            required
+            ></v-select>
+            <!-- TODO:      put 'available' lobbys in select tag
+                -->
+
+            <v-btn>submit</v-btn>
+            <!--  TODO:     Form submition naar /game/hotpotato?lobby=lobbyname
+                                                POST: username
+            -->
+        </v-form>
+        
     </div>
 </template>
 
@@ -23,24 +39,22 @@ export default {
       currentRoute: window.location.pathname,
       lobbys: [
         {
-          name: "lobby1",
+          text: "lobby1",
           currentplayers: 2
         },
         {
-          name: "lobby2",
+          text: "lobby2",
           currentplayers: 0
         }
-      ]
+      ],
+      form_info: {
+        name: "",
+        selectedLobby: {}
+      }
     };
   },
   methods: {
-    greet: function(e) {
-      var name = prompt("Please enter your name:", "Your name");
-      if (name != null) {
-        //window.location = "/game/hotpotato?=" + name;
-        console.log(e);
-      }
-    }
+    submit: function(e) {}
   }
 };
 </script>
