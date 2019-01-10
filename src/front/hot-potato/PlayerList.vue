90bae9510cfe6211ab40b18fec0c9ac49be4e251
Dekker,Lars L <lars.dekker@student.fontys.nl>
2019-01-10 12:17:04 +0100

    Martin's git probleem gefixed: Playerlisttoggle

--- src/front/hot-potato/PlayerList.vue
+++ src/front/hot-potato/PlayerList.vue
@@ -1,60 +1,72 @@
 
 
 <template>
-<v-container >
+  <v-container>
+    <v-flex>
+      <v-card class="elevation-10">
+        <v-toolbar color="light-blue" dark>
+          <v-toolbar-side-icon v-on:click="toggleList"></v-toolbar-side-icon>
 
+          <v-toolbar-title>Players</v-toolbar-title>
 
-<v-flex>
- <v-card class="elevation-10">
-    <v-toolbar color="light-blue" dark>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-  
-            <v-toolbar-title>Players</v-toolbar-title>
-  
-            <v-spacer></v-spacer>
-  
-            <v-btn icon>
-              <v-icon>info</v-icon>
-            </v-btn>
-          </v-toolbar>
- <v-list>
-            <v-list-tile
-               v-for="playerid in id" :key="playerid">
-          
-              <v-list-tile-content>
-                <v-list-tile-title v-text="playerid" v-bind:id="playerid">
-                  
-                  {{playerid}}
-                  </v-list-tile-title>
-              </v-list-tile-content>
-              
-               <v-list-tile-avatar v-if="playerid==playerWithBomb">
-                <img src="https://i.imgur.com/7WBNNfa.png">
-              </v-list-tile-avatar>
-            </v-list-tile>
-          </v-list>
-</v-card>
-</v-flex>
+          <v-spacer></v-spacer>
 
-</v-container>
+          <v-btn icon>
+            <v-icon>info</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-list>
+          <v-list-tile
+            v-for="playerid in id"
+            :key="playerid"
+            v-if="enabled==true || playerid==playerWithBomb"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title v-text="playerid" v-bind:id="playerid">{{playerid}}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-avatar v-if="playerid==playerWithBomb">
+              <img src="https://i.imgur.com/7WBNNfa.png">
+            </v-list-tile-avatar>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-container>
 </template>
 
 
 <script>
 export default {
   name: "PlayerList",
-  props: ["id","playerWithBomb"],
-  computed:
-  {
-    hasBomb:function(inputid)
-    {
-      if(inputid===this.$props.playerWithBomb)
-      {
-        console.log("Id van speler met bom: "+this.$props.playerWithBomb+": id van speler: "+id)
+  props: ["id", "playerWithBomb", "listEnabled"],
+  data() {
+    return {
+      enabled: true
+    };
+  },
+  computed: {
+    hasBomb: function(inputid) {
+      if (inputid === this.$props.playerWithBomb) {
+        console.log(
+          "Id van speler met bom: " +
+            this.$props.playerWithBomb +
+            ": id van speler: " +
+            id
+        );
         return true;
       }
     }
-}
+  },
+  methods: {
+    toggleList: function() {
+      if (this.enabled == true) {
+        this.enabled = false;
+      } else {
+        this.enabled = true;
+      }
+    }
+  }
 
   //players: [],
 };
